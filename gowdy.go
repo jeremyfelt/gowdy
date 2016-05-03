ad90b4aff90b002a1755c96666d899ed9bb4161c
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-02 21:44:10 -0700

    Notes about function parameters
    And apparently `//` works for comments too... :)

--- gowdy.go
+++ gowdy.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+// Type can be supplied after each parameter...
+func add(x int, y int) int {
+	return x + y
+}
+// ...Or if both parameters are the same type, at the end.
+func sum(x, y int) int {
+	return x + y
+}
+
 func main() {
-	fmt.Printf("Howdy, gowdy!\n")
+	fmt.Println(add(123,234))
+	fmt.Println(sum(234,123))
 }
