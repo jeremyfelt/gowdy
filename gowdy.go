5f09faadf27e917fa51ed9a7cede98a5e65fc752
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-02 21:52:26 -0700

    A function can return any number of results

--- gowdy.go
+++ gowdy.go
@@ -11,7 +11,20 @@ func sum(x, y int) int {
 	return x + y
 }
 
+// A function can return any number of results, which seems super
+// cool. I know I've had to create arrays before just to get two
+// pieces of data back. I'm pretty sure multiline comments work now.
+func addsum(a, b int) (int, int, int) {
+	var x int
+	x = a + b
+	return a, b, x
+}
+
 func main() {
+	a, b, c := addsum(add(123,234), 124)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
 	fmt.Println(add(123,234))
 	fmt.Println(sum(234,123))
 }
