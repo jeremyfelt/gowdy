b4e8d2771e8cb5fffe2a276ba6a8057d9ee6f7f2
Jeremy Felt <jeremy.felt@gmail.com>
2016-05-02 21:59:55 -0700

    Continuing to learn rules about parameters :)

--- gowdy.go
+++ gowdy.go
@@ -20,11 +20,33 @@ func addsum(a, b int) (int, int, int) {
 	return a, b, x
 }
 
+func sumadd(a, b int) (x, y int) {
+	x = a + b
+	y = b - a
+	return
+}
+
 func main() {
 	a, b, c := addsum(add(123,234), 124)
 	fmt.Println(a)
 	fmt.Println(b)
 	fmt.Println(c)
-	fmt.Println(add(123,234))
+
+	// If you declare something like "second", you have to use it otherwise
+	// a build error will fire.
+	first, second := sumadd(321,320)
+	fmt.Println("First: ", first)
+	fmt.Println("Second: ", second)
+
+	// If I print sumadd directly, both return values output
+	fmt.Println(sumadd(321,320))
+
+	// However, something like this won't work because it treats this as
+	// multi-value in single-value context. ?
+	//fmt.Println("First and Second: ", sumadd(321,320))
+
+	// This does work though because a single value is returned.
+	fmt.Println("Add: ", add(123,234))
+
 	fmt.Println(sum(234,123))
 }
